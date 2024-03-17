f1b35aa5915dd186ffe7d0aa3ecbbb0dda3cad4e
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2024-03-17 00:03:39 -0700

    GH-13

--- config/playground-matrix.php
+++ config/playground-matrix.php
@@ -3,6 +3,7 @@
 declare(strict_types=1);
 
 return [
+    'about' => (bool) env('PLAYGROUND_MATRIX_ABOUT', true),
     'load' => [
         'migrations' => (bool) env('PLAYGROUND_MATRIX_LOAD_MIGRATIONS', false),
     ],
